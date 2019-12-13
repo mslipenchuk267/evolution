6962f74b3c2b13296229d5fc5ab865bafe230caa
Matthew Slipenchuk <tuf91673@temple.edu>
2019-12-13 11:02:17 -0500

    evolution functions update

--- evolution.c
+++ evolution.c
@@ -6,9 +6,8 @@
 #include "h/evolution.h"
 #include "xmalloc.h"
 #include "array.h"
-#include "read.h"
-#include "write.h"
-#include "linked-list.h"
+#include "h/read.h"
+#include "h/linked-list.h"
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 struct point {
     int i;
@@ -34,6 +33,122 @@ static void evolve(struct world *world, unsigned int n);
 static void evolve_with_figs(struct world *world, unsigned int n);
 static void show_usage(char *progname);
 
+static void free_herd(conscell *herd) {
+    for (conscell *p = herd; p != NULL; p = p->next) {
+        free(p->data);
+    }
+    ll_free(herd);
+}
+
+static conscell *remove_the_dead(conscell *herd) {
+    conscell *dead = NULL;
+    herd = ll_filter(herd, dead_or_alive, &dead);
+    free_herd(dead);
+    return herd;
+}
+
+static struct animal *clone(struct animal *old) {
+    struct animal *new = xmalloc(sizeof *new);
+    new->i = old->i;
+    new->j = old->j;
+    new->d = old->d;
+    new->e = old->e;
+    int i;
+    for (i = 0; i < 9; i++) {
+        new->genes[i] = old->genes[i];
+    }
+    return new;
+}
+
+static void turn(struct animal *animal) {
+
+}
+
+static void move(struct world *world, struct animal *animal) {
+    struct {
+        int i;
+        int j;
+    } motion_vectors[] = {
+            {1,0},
+            {1,1},
+            {0,1},
+            {-1, 1},
+            {-1, 0},
+            {-1, -1},
+            {0, -1},
+            {1, -1}
+    };
+    int i = animal->i + motion_vectors[animal->d].i;
+    int j = animal->j + motion_vectors[animal->d].j;
+    if (i < 0) {
+        i += world->world_h;
+    } else if (i >= world->world_h) {
+        i -= world->world_h;
+    }
+    if (j < 0) {
+        j += world->world_w;
+    } else if (j >= world->world_w) {
+        j -= world->world_w;
+    }
+    animal->i = i;
+    animal->j = j;
+}
+
+static void add_plants(struct world *world) {
+    int i = random(world->world_h);
+    int j = random(world->world_w);
+    world->plants[i][j]++; // Deposit morsel of food
+
+    if (world->eden_h > 0 && world->eden_w > 0) {
+        i;
+        j;
+        world->plants[i][j]++;
+    }
+}
+
+static void reproduce(struct world *world, struct animal *animal) {
+    animal->e /= 2;
+    struct animal *new = clone(animal);
+    mutate(new->genes);
+    world->herd = ll_push(world->herd, new);
+}
+
+static void update_world(struct world *world) {
+    world->herd = remove_the_dead(world->herd);
+    for (conscell *p = world->herd; p != NULL; p = p->next) {
+        struct animal *a = p->data;
+        turn(a);
+        move(world, a);
+        feed(world, a);
+        a->e--;
+        if(a->e >= world->reproduction_threshold) {
+            reproduce(world, a);
+        }
+    }
+    add_plants(world);
+}
+
+static void evolve_with_figs(struct world *world, unsigned int n){
+    char buf[16];
+    for (unsigned long int i = 0; i < n; i++) {
+        sprintf(buf, "fig%04lu.eps", i);
+        world_to_eps(world, buf);
+        if (i < n - 1) {
+            update_world(world);
+        }
+    }
+}
+
+static void evolve(struct world *world, unsigned int n){
+    char buf[16];
+    for (unsigned long int i = 0; i < n; i++) {
+        sprintf(buf, "World Update #: %d", i);
+        if (i < n - 1) {
+            update_world(world);
+        }
+    }
+}
+
 int main(int argc, char **argv) {
     unsigned long int n;
     unsigned long int f = 0;
@@ -48,10 +163,35 @@ int main(int argc, char **argv) {
         goto cleanup;
     }
 
+    if (sscanf(argv[1], "%lu", &n) != 1) {
+        show_usage(argv[0]);
+        goto cleanup;
+    }
+
+    if (argc > 2 && sscanf(argv[2], "%lu", &f) != 1) {
+        show_usage(argv[0]);
+        goto cleanup;
+    }
+
+    if (!read_wdf(world)) {
+        goto cleanup;
+    }
+    initialize_plants(world);
+    evolve(world, n);
+    if (f > 0) {
+        evolve_with_figs(world, f);
+    }
+
+    struct point eden_center;
+    eden_center.i = (world->world_h - 1)/2;
+    eden_center.j = (world->world_w - 1)/2;
+    world->herd = ll_sort(world->herd, nearer_the_eden, &eden_center);
+    write_wdf(world);
+    exit_status = EXIT_SUCCESS;
+
     cleanup:
         free_matrix(world->plants);
         free_herd(world->herd);
         return exit_status;
-
 
 }
